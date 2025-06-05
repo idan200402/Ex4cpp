cc4621797c8c8a719acf40e3a8794836622edd57
Idan Shumski <idan.shumski@gmail.com>
2025-06-05 09:31:12 +0300

    added documentation to Mycontainer.hpp and to BaseIteratoe.hpp

--- include/Iterators/BaseIterator.hpp
+++ include/Iterators/BaseIterator.hpp
@@ -4,41 +4,81 @@
 #include <memory>
 #include <stdexcept>
 namespace ex4 {
+// Forward declaration of MyContainer to use it in BaseIterator
 template <typename T>
 class MyContainer;
 template <typename T>
+/**
+ * @class BaseIterator
+ * @brief A base iterator class that provides common functionality for iterators in the MyContainer class.
+ */
 class BaseIterator {
 protected:
+    // The view is a vector that holds the elements to be traversed.
     std::vector<T> view;
+    // The current index of the iterator in the view.
     size_t currentIndex;
+    // The end index of the view, used to determine when the iterator has reached the end.
     size_t endIndex;
 
 public:
+    /**
+     * @brief Default constructor for BaseIterator.
+     * Initializes the iterator with an empty view and sets the current index and end index to 0.
+     */
     BaseIterator() : currentIndex(0), endIndex(0) {}
-
+    /**
+     * @brief Constructor for BaseIterator that initializes the iterator with a given traversal view.
+     * @param traversalView A vector containing the elements to be traversed.
+     * @param startIndex The starting index for the iterator, default is 0.
+     */
     BaseIterator(std::vector<T> traversalView, size_t startIndex = 0)
         : view(std::move(traversalView)), currentIndex(startIndex), endIndex(view.size()) {}
-
+    /**
+     * @brief pure virtual destructor for BaseIterator.
+     * This ensures that we wont be able to instantiate BaseIterator directly, as it is meant to be a base class for other iterators.
+     */
     virtual ~BaseIterator() = default;
-
+    /**
+     * @brief method to dereference the iterator.
+     * @return A reference to the current element in the view.
+     * @throw std::out_of_range if the current index is past the end of the view.
+     */
     const T& operator*() const {
         if (currentIndex >= endIndex) {
             throw std::out_of_range("Dereferencing past the end");
         }
         return view[currentIndex];
     }
-
+    /**
+     * @brief Overloaded pre-increment operator to move the iterator to the next element.
+     * @return A reference to the current iterator after incrementing.
+     */
     BaseIterator& operator++() {
         ++currentIndex;
         return *this;
     }
-
+    /**
+     * @brief Overloaded not equal operator to compare two iterators. This operator checks if the current index and view of two iterators are equal.
+     * @param other The other iterator to compare with.
+     * @return true if the iterators are not equal, false otherwise.
+     */
     bool operator!=(const BaseIterator& other) const {
         return !(*this == other);
     }
+    /**
+     * @brief Overloaded equality operator to compare two iterators.
+     * @param other The other iterator to compare with.
+     * @return true if the current index and view of both iterators are equal, false otherwise.
+     */
     bool operator==(const BaseIterator& other) const {
         return currentIndex == other.currentIndex && view == other.view;
     }
+    /**
+     * @brief Overloaded pre-decrement operator to move the iterator to the previous element.
+     * @return A reference to the current iterator after decrementing.
+     * @throw std::out_of_range if the current index is already at the beginning of the view.
+     */
     BaseIterator& operator--() {
         if (currentIndex == 0) {
             throw std::out_of_range("Cannot decrement past the beginning");
@@ -46,6 +86,10 @@ public:
         --currentIndex;
         return *this;
     }
+    /**
+     * @brief Method to get the view of the iterator.
+     * @return A vector containing the elements that the iterator is traversing.
+     */
     std::vector<T> getView() const {
         return view;
     }
--- include/MyContainer.hpp
+++ include/MyContainer.hpp
@@ -15,10 +15,15 @@
 
 
 namespace ex4 {
-
+/**
+ * @class MyContainer 
+ * @brief A generic container class that holds elements of type T and provides various iterators for traversing the elements in different orders.
+ */
 template <typename T = int> class MyContainer{
     private:
+        // the internal storage for the container using a generic type T std::vector<T> to hold the elements.
         std::vector<T> data;
+        // A private method to print the elements of the container to an output stream , its a helper function for the operator<< overload.
         void print(std::ostream& os = std::cout) const{
             for (const auto& elem : data) {
                 os << elem << ' ';
@@ -26,61 +31,140 @@ template <typename T = int> class MyContainer{
             os << std::endl;
         }
     public:
+        /**
+         * @brief Default constructor for MyContainer.
+         * Initializes an empty container.
+         * This constructor allows the creation of a MyContainer object without any initial elements.
+         * It is useful when you want to add elements later.
+         */
         MyContainer() = default;
+        /**
+         * @brief add an element to the container.
+         * @param value The value to be added to the container , has to be of type T.
+         */
         void addElement(const T& value){
             data.push_back(value);    
         }
+        /**
+         * @brief remove an element from the container , if this element has multiple instances, all instances will be removed.
+         * @param value The value to be removed from the container , has to be of type T.
+         * @throw std::runtime_error if the element does not exist in the container.
+         */
+         
         void removeElement(const T& value){
+            //if the element is found , we will move them to the end of the vector and then erase them.
             auto newEnd = std::remove(data.begin(), data.end(), value);
+            //if the end iterator stays the same  , and we did not find the element, we will throw an error.
             if (newEnd == data.end() && std::find(data.begin(), data.end(), value) == data.end()) {
                 throw std::runtime_error("This element does not exist in the container");
             }
+            //erase the elements from the new end (the begin of the elements that we want to remove) to the end of the vector.
             data.erase(newEnd, data.end());
         }
+        /**
+         * @brief Get the size of the container.
+         * @return The number of elements in the container.
+         */
         size_t size() const {
             return data.size();
         }
+        /**
+         * @brief Overloaded output stream operator to print the container's elements.
+         * @param os The output stream to which the elements will be printed.
+         * @param container The MyContainer object whose elements are to be printed.
+         * @return The output stream after printing the elements.
+         */
         template <typename U>
         friend std::ostream& operator<<(std::ostream& os, const MyContainer<U>& container);
 
+        /**
+         * @brief Get the data stored in the container.
+         * @return A constant reference to the vector containing the elements of type T.
+         */
         const std::vector<T>& getData() const {
             return data;
         }
+        /**
+         * @brief this method creates an begin iterator for the container , in a regular order.
+         * @return An iterator that points to the first element of the container.
+         */
         Order<T> begin_order() const {
             return Order<T>::make(*this).first;
         }
+        /**
+         * @brief this method creates an end iterator for the container , in a regular order.
+         * @return An iterator that points to the end of the container.
+         */
         Order<T> end_order() const {
             return Order<T>::make(*this).second;
         }
+        /**
+         * @brief Creates iterator for the container in ascending order.
+         * @return An iterator that points to the first element of the container in ascending order.
+         */
         AscendingOrder<T> begin_ascending_order() const {
             return AscendingOrder<T>::make(*this).first;
         }
-
+        /**
+         * @brief Creates an end iterator for the container in ascending order.
+         * @return An iterator that points to the end of the container in ascending order.
+         */
         AscendingOrder<T> end_ascending_order() const {
             return AscendingOrder<T>::make(*this).second;
         }
-
+        /**
+         * @brief Creates an iterator for the container in descending order.
+         * @return An iterator that points to the first element of the container in descending order.
+         */
         DescendingOrder<T> begin_descending_order() const {
             return DescendingOrder<T>::make(*this).first;
         }
+        /**
+         * @brief Creates an end iterator for the container in descending order.
+         * @return An iterator that points to the end of the container in descending order.
+         */
         DescendingOrder<T> end_descending_order() const {
             return DescendingOrder<T>::make(*this).second;
         }
+        /**
+         * @brief Creates an iterator for the container in side cross order.
+         * @return An iterator that points to the first element of the container in side cross order.
+         */
         SideCrossOrder<T> begin_side_cross_order() const {
             return SideCrossOrder<T>::make(*this).first;
         }
+        /**
+         * @brief Creates an end iterator for the container in side cross order.
+         * @return An iterator that points to the end of the container in side cross order.
+         */
         SideCrossOrder<T> end_side_cross_order() const {
             return SideCrossOrder<T>::make(*this).second;
         }
+        /**
+         * @brief Creates an iterator for the container in reverse order.
+         * @return An iterator that points to the first element of the container in reverse order.
+         */
         ReverseOrder<T> begin_reverse_order() const {
             return ReverseOrder<T>::make(*this).first;
         }
+        /**
+         * @brief Creates an end iterator for the container in reverse order.
+         * @return An iterator that points to the end of the container in reverse order.
+         */
         ReverseOrder<T> end_reverse_order() const {
             return ReverseOrder<T>::make(*this).second;
         }
+        /**
+         * @brief Creates an iterator for the container in middle out order.
+         * @return An iterator that points to the first element of the container in middle out order.
+         */
         MiddleOutOrder<T> begin_middle_out_order() const {
             return MiddleOutOrder<T>::make(*this).first;
         }
+        /**
+         * @brief Creates an end iterator for the container in middle out order.
+         * @return An iterator that points to the end of the container in middle out order.
+         */
         MiddleOutOrder<T> end_middle_out_order() const {
             return MiddleOutOrder<T>::make(*this).second;
         }
@@ -88,6 +172,7 @@ template <typename T = int> class MyContainer{
        
         
     };
+// Overloaded output stream operator for MyContainer , outside the class definition we implement the operator<< function.
 template <typename T>
 std::ostream& operator<<(std::ostream& os, const MyContainer<T>& container) {
     container.print(os);
